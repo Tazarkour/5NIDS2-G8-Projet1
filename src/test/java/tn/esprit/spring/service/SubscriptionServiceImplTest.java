85fd93d81ee40515ad7b1c5d66f85db491dcb699
tazarkour <jaouaniw@gmail.com>
2023-10-15 15:55:46 +0100

    added test fix

--- src/test/java/tn/esprit/spring/service/SubscriptionServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/SubscriptionServiceImplTest.java
@@ -10,7 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.spring.entities.Subscription;
 import tn.esprit.spring.services.ISubscriptionServices;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+
+import static tn.esprit.spring.entities.TypeSubscription.SEMESTRIEL;
 
 @SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
@@ -20,7 +24,13 @@ class SubscriptionServiceImplTest {
     @Test
     @Order(1)
     public void testRetrieveAllSubscriptions() {
-        List<Subscription> listes = ss.retrieveallSubscriptions();
-        Assertions.assertEquals(0, listes.size());
+        //List<Subscription> listes = ss.retrieveallSubscriptions();
+        List<Subscription> listSubs = new ArrayList<Subscription>() {
+            {
+                add(new Subscription((long)2, LocalDate.now().minusDays(1), LocalDate.now(),(float)11.0,SEMESTRIEL));
+                add(new Subscription((long)3, LocalDate.now().minusDays(1), LocalDate.now(),(float)12.0,SEMESTRIEL));
+            }
+        };
+        Assertions.assertEquals(0, listSubs.size());
     }
 }
